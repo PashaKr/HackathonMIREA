57dfce3d6db0094f8ba4aede42619c31ce01f45a
PashaKr <46005429+PashaKr@users.noreply.github.com>
2024-12-07 18:53:00 +0300

    first commit

---
+++ src/main/java/com/example/hackathonmirea/HackathonMireaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hackathonmirea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonMireaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonMireaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hackathonmirea/HackathonMireaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathonmirea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonMireaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
