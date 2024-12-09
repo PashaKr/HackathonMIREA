a4902edc3851112b0bb46a876feb692cb3b9449a
grentte <danr.zh2@gmail.com>
2024-12-09 19:14:21 +0300

    added visualization of formulas in real time

--- src/main/java/com/example/hackathonmirea/controllers/HomeController.java
+++ src/main/java/com/example/hackathonmirea/controllers/HomeController.java
@@ -14,9 +14,4 @@ public class HomeController {
         return "index"; // Отображение формы
     }
 
-    @PostMapping("/render")
-    public String renderLatex(@RequestParam("latexInput") String latexInput, Model model) {
-        model.addAttribute("latexInput", latexInput);
-        return "index"; // Возвращаем ту же страницу с визуализацией формулы
-    }
 }
